b4b7897978c58ea324f96eb3f7f4153420b9dd1b
eswedwin <eswedwin@gmail.com>
2013-07-14 19:19:13 +0000

    web.js -> read from file index 3rd try

--- web.js
+++ web.js
@@ -18,8 +18,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-fs.readFileSync(filename, function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
