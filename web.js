1f1c4b96f91c3ceedb1c053420cb3023113ea972
eswedwin <eswedwin@gmail.com>
2013-07-15 00:33:31 +0000

    web.js -> read from file index nth try

--- web.js
+++ web.js
@@ -3,15 +3,15 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var filename = "index.html";
+var data = new Buffer(200, 'utf8');
 
 app.get('/', function(request, response) {
-  var data = new Buffer(200, 'utf8');
-  fs.readFileSync(filename, encoding ='utf8',function (err, data) {
-    if (err) throw err;
-    console.log(data);
-    // buf.write(data);
-  });
-  response.send(data.toString('utf8'));
+  response.send(
+    fs.readFileSync(filename, encoding ='utf8',function (err, data) {
+      if (err) {throw err;}
+      console.log(data);
+      return data.toString();})
+  );
 });
 
 var port = process.env.PORT || 5000;
