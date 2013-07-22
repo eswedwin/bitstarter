3ebcf4a1c87e3f1827f118cb52ac08a83d20ed8d
eswedwin <eswedwin@gmail.com>
2013-07-22 18:55:56 +0000

    rtfm - fixed readfilesync & add static/ for direct access

--- web.js
+++ web.js
@@ -3,18 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var filename = "index.html";
-var data = new Buffer(200, 'utf8');
+//var data = new Buffer(256, 'utf8');
+
+app.use("/static",express.static(__dirname+"/static"));
 
 app.get('/', function(request, response) {
   response.send(
-    fs.readFileSync(filename, encoding ='utf8',function (err, data) {
-      if (err) {throw err;}
-      //console.log(data);
-      return data.toString();})
-  );
+    fs.readFileSync(filename).toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
